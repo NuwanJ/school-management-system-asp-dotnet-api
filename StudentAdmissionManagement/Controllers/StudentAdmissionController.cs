8243293d61bceb016fd4fec5164fe54e5e90ffc6
Nuwan Jaliyagoda <nuwanjaliyagoda@gmail.com>
2024-07-20 14:07:59 +0530

    Port changes and code clenups

--- StudentAdmissionManagement/Controllers/StudentAdmissionController.cs
+++ StudentAdmissionManagement/Controllers/StudentAdmissionController.cs
@@ -1,38 +1,6 @@
-﻿//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentAdmissionManagement;
 
-//namespace StudentAdmissionManagement.Controllers
-//{
-
-//    [ApiController]
-//    [Route("api/[controller]")]
-//    public class StudentAdmissionController : Controller
-//    {
-//        [HttpGet]
-//        public IEnumerable<StudentAdmissionDetailsModel> Get()
-//        {
-//            StudentAdmissionDetailsModel admObj1 = new StudentAdmissionDetailsModel();
-//            StudentAdmissionDetailsModel admObj2 = new StudentAdmissionDetailsModel();
-
-//            admObj1.StudentID = 1;
-//            admObj1.StudentName = "Adam";
-//            admObj1.StudentClass = "X";
-//            admObj1.DateOfJoining = DateTime.Now;
-
-//            admObj2.StudentID = 2;
-//            admObj2.StudentName = "Brad";
-//            admObj2.StudentClass = "IX";
-//            admObj2.DateOfJoining = DateTime.Now;
-
-//            List<StudentAdmissionDetailsModel> listOfObj = new List<StudentAdmissionDetailsModel> { admObj1, admObj2 };
-
-//            return listOfObj;
-//        }
-
-//    }
-//}
-
-using Microsoft.AspNetCore.Mvc;
 namespace StudentAdmissionManagement.Controllers
 {
     [Route("api/[controller]")]
--- StudentAttendenceManagement/Controllers/StudentAttendanceController.cs
+++ StudentAttendenceManagement/Controllers/StudentAttendanceController.cs
@@ -1,46 +1,4 @@
-﻿//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using StudentAttendenceManagement;
-
-//namespace StudentAttendenceManagement.Controllers
-//{
-//    [ApiController]
-//    [Route("api/[controller]")]
-//    public class StudentAttendanceController : ControllerBase
-//    {
-//        [HttpGet]
-//        public IEnumerable<StudentAttendanceDetailsModel> Get()
-//        {
-//            StudentAttendanceDetailsModel atdObj1 = new StudentAttendanceDetailsModel();
-//            StudentAttendanceDetailsModel atdObj2 = new StudentAttendanceDetailsModel();
-
-//            atdObj1.StudentID = 1;
-//            atdObj1.StudentName = "Adam";
-//            atdObj1.AttendencePercentage = 83.2;
-
-
-//            atdObj2.StudentID = 2;
-//            atdObj2.StudentName = "Brad";
-//            atdObj2.AttendencePercentage = 71.02;
-
-//            List<StudentAttendanceDetailsModel> listOfObj = new List<StudentAttendanceDetailsModel> { atdObj1, atdObj2 };
-
-//            return listOfObj;
-//        }
-
-//    }
-//}
-
-
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-
-
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using StudentAttendenceManagement;
 
 namespace StudentAttendanceManagement.Controllers
