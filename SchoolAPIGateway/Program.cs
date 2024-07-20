46bcd7908583f57c55b00966ad0978bc40d18827
Nuwan Jaliyagoda <nuwanjaliyagoda@gmail.com>
2024-07-20 13:25:02 +0530

    Working application

--- SchoolAPIGateway/Program.cs
+++ SchoolAPIGateway/Program.cs
@@ -1,17 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddOcelot(builder.Configuration);
+
+
+// Load the Ocelot configuration file
+builder.Configuration.AddJsonFile("ocelot.json");
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
-app.UseHttpsRedirection();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Ocelot API Gateway");
+    });
+});
+
+// Configure the HTTP request pipeline.
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
